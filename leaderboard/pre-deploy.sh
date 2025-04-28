ab720634cc39067be9168787c564e7835acd9ea7
Jagjeevan <83103469+JagjeevanAK@users.noreply.github.com>
2025-04-28 19:16:46 +0530

    Update pre-deploy.sh

--- leaderboard/pre-deploy.sh
+++ leaderboard/pre-deploy.sh
@@ -1,4 +1,4 @@
-: "${LEADERBOARD_REPO:=https://github.com/OpenFoodFacts/leaderboard.git}"
+: "${LEADERBOARD_REPO:=https://github.com/JagjeevanAK/leaderboard-data.git}"
 
 git init
 git remote add --mirror=fetch origin ${LEADERBOARD_REPO}
